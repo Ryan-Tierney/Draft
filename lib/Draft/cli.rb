289f66e067412c93fb6a77e87d8377ab59979dd9
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-06 18:56:34 +0000

    stubbed CLI

---
+++ lib/Draft/cli.rb
@@ -0,0 +1,11 @@
+class Draft::CLI 
+  
+  def call 
+    puts "Welcome! This is a list of the top 10 players for the 2020 NFL draft".colorize(:light_red)
+    gets_player_list 
+  end 
+  
+  def gets_player_list
+    
+  end 
+end 
---
+++ lib/environment.rb
@@ -0,0 +1,12 @@
+require_relative "Draft/version"
+require_relative "Draft/cli"
+require "nokogiri"
+require "pry"
+require "open-uri"
+require "colorize"
+
+
+module Draft
+  class Error < StandardError; end
+  # Your code goes here...
+end
