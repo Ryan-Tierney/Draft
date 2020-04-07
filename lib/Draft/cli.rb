1ec6d846831ed675434a0ba38f2ac0e2394297ea
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-07 19:57:48 +0000

    cli complete

--- lib/Draft/cli.rb
+++ lib/Draft/cli.rb
@@ -5,7 +5,6 @@ class Draft::CLI
     gets_player_list 
     gives_player_list
     gets_user_selection
-    show_summary_for_player(chosen_player)
   end 
   
   def gets_player_list
@@ -13,23 +12,27 @@ class Draft::CLI
   end 
   
   def gives_player_list 
-    @prospect.each_with_index do |player, index|
-      puts "#{index +1}. #{player}"
+      puts "\nChoose a player to see his summary!\n".colorize(:light_blue)
+    @prospect.each.with_index(1) do |player, index|
+      puts "#{index}. #{player}"
     end 
   end 
   
   def gets_user_selection 
     chosen_player = gets.strip.to_i
-    show_summary_for_player(chosen_player) if valid_input?(chosen_player, @prospect) 
+    show_summary_for_player(chosen_player) if valid_input(chosen_player, @prospect) 
   end 
   
-  def valid_input?(input, data)
+  def valid_input(input, data)
     if input.to_i <= data.length && input.to_i > 0 
     end 
   end 
   
   def show_summary_for_player(chosen_player)
-    player = @prospect[chosen_player]
+    player = @prospect[chosen_player -1]
     puts "here is the info for #{player}"
+    Draft::Prospect.all.each do | prospect |
+      puts prospect.summary
+    end 
   end 
 end 
