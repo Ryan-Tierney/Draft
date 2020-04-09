a771e96e5d68e4fff56630b2650f509771ee1368
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-09 05:40:31 +0000

    update scraper and prospect class

--- lib/Draft/cli.rb
+++ lib/Draft/cli.rb
@@ -8,31 +8,29 @@ class Draft::CLI
   end 
   
   def gets_player_list
-    @prospect = ["Joe Burrow", "Chase Young", "Jeff Okudah", "Derrick Brown"]
+    Draft::Prospect.new("Joe Burrow", "Summary")
+    @prospect = Draft::Prospect.all 
   end 
   
   def gives_player_list 
       puts "\nChoose a player to see his summary!\n".colorize(:light_blue)
     @prospect.each.with_index(1) do |player, index|
-      puts "#{index}. #{player}"
+      puts "#{index}. #{player.name}"
     end 
   end 
   
   def gets_user_selection 
     chosen_player = gets.strip.to_i
-    show_summary_for_player(chosen_player) if valid_input(chosen_player, @prospect) 
+    show_summary_for(chosen_player) if valid_input(chosen_player, @prospect) 
   end 
   
   def valid_input(input, data)
-    if input.to_i <= data.length && input.to_i > 0 
-    end 
+    input.to_i <= data.length && input.to_i > 0 
   end 
   
-  def show_summary_for_player(chosen_player)
+  def show_summary_for(chosen_player)
     player = @prospect[chosen_player -1]
-    puts "here is the info for #{player}"
-    Draft::Prospect.all.each do | prospect |
-      puts prospect.summary
-    end 
+    puts "#{player.name}"
+    puts "#{player.summary}"
   end 
 end 
--- lib/Draft/prospect.rb
+++ lib/Draft/prospect.rb
@@ -1,2 +1,21 @@
 class Draft::Prospect
+  
+  attr_accessor :name, :summary
+  
+  @@all = [] 
+  
+  def initialize(name, summary)
+    @name = name 
+    @summary = summary 
+    save 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def save
+   @@all << self 
+  end 
+  
 end 
