847699da368e1978be5f5a2793745a5e359b0c0a
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-10 02:10:34 +0000

    update prospect file

--- lib/Draft/prospect.rb
+++ lib/Draft/prospect.rb
@@ -1,18 +1,24 @@
 class Draft::Prospect
   
-  attr_accessor :name, :summary
+  attr_accessor :name
+  attr_writer :summary
   
   @@all = [] 
   
-  def initialize(name, summary)
+  def initialize(name)
     @name = name 
-    @summary = summary 
+    @summary = " " 
     save 
   end 
   
   def self.all 
     Draft::Scraper.scrape_prospect if @@all.empty?
     @@all 
+  end 
+  
+  def summary 
+    Draft::Scraper.scrape_prospect if @summary.empty?
+    @summary
   end 
   
   def save
