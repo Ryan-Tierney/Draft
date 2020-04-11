8991a10ec3986e0773f3f56a35c8a0111d54025c
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-11 19:03:25 +0000

    update scraper class

--- lib/Draft/scraper.rb
+++ lib/Draft/scraper.rb
@@ -1,13 +1,13 @@
 class Draft::Scraper
  
- def scrape_prospect
+ def self.scrape_prospect
   doc = Nokogiri::HTML(open("http://www.nfl.com/news/story/0ap3000001105591/article/daniel-jeremiahs-top-50-2020-nfl-draft-prospect-rankings-30"))  
   prospect = doc.css("div.pr-item")
     prospect.each do |p|
     name = p.css("p.team-info").text.strip
     Draft::Prospect.new(name)
-    summary = p.css("p.pr-body").text.strip
-    Draft::Prospect.new(summary)
+    #summary = p.css("p.pr-body").text.strip
+    #Draft::Prospect.new(summary)
    end 
  end
 end 
